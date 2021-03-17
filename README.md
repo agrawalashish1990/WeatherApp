2af18538dd7ed8ed0009b7eeae10da9eac5518d0
agrawalashish1990 <agrawal.ashish1990@gmail.com>
2021-03-17 09:54:01 +0530

    Added READMe file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # WeatherApp
 WeatherApp
+
+To run this APP please put KEY_WEATHER_API in Constants.kt
+
+
+Used Third party libraries-
+
+1. pub.devrel:easypermissions
+2. com.github.bumptech.glide:glide
+3. com.squareup.retrofit2:retrofit
+
+
--- app/src/main/java/com/dev/ashish/weather/utils/Constants.kt
+++ app/src/main/java/com/dev/ashish/weather/utils/Constants.kt
@@ -6,7 +6,7 @@ package com.dev.ashish.weather.utils
 
 object Constants {
 
-    const val KEY_WEATHER_API = "18aa614c2417fc06a12ed59b6e0c525d"
+    const val KEY_WEATHER_API = ""
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val IMAGE_BASE_URL = "http://openweathermap.org/img/wn/"
     const val STATUS_CODE_UN_AUTHORIZED = 401
